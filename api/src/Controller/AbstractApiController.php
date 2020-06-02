df7e93408de6d96e7da58cf855b0b926215d1329
Oleg <more8@ukr.net>
2020-06-02 18:40:46 +0300

    TP-15: Set Swagger to the api

---
+++ api/src/Controller/AbstractApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+abstract class AbstractApiController extends AbstractController
+{
+    protected function getJson(Request $request) 
+    {
+        $data = json_decode($request->getContent(), true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new BadRequestHttpException('invalid json body: ' . json_last_error_msg());
+        }
+        return $data;
+    }
+}
--- api/src/Controller/AuthController.php
+++ api/src/Controller/AuthController.php
@@ -2,19 +2,185 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Swagger\Annotations as SWG;
 
-class AuthController extends AbstractController
+class AuthController extends AbstractApiController
 {
     /**
-     * 
-     * @Route("/register", name="auth", methods={"POST"})
+     * @Route("/register", name="register", methods={"POST"})
+     * @SWG\Post(
+     *    tags={"Authorization"},
+     *    summary="Register new user",
+     *     @SWG\Parameter(
+     *         name="body",
+     *         in="body",
+     *         description="Request with user email",
+     *         required=true,
+     *         @SWG\Schema(
+     *             @SWG\Property(property="email", type="string", example="nickname@server.com")
+     *         )
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Start registration proccess"
+     *     ),
+     * )
      */
-    public function register()
+    public function register(Request $request)
     {
+        $data = $this->getJson($request);
+
         return $this->json([
-            "success" => true
+            "success" => $data["email"]
         ]);
     }
+
+    /**
+     * @Route("/register/complete", name="registerComplete", methods={"POST"})
+     * @SWG\Post(
+     *    tags={"Authorization"},
+     *    summary="Complete register with email code",
+     *     @SWG\Parameter(
+     *         name="emailCode",
+     *         in="body",
+     *         description="Request with email code",
+     *         required=true,
+     *         @SWG\Schema(
+     *             @SWG\Property(property="email_code", type="string", example="nickname@server.com")
+     *         )
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Complete registration proccess"
+     *     ),
+     * )
+     */
+    public function registerComplete(Request $request)
+    {
+        return $this->json([
+            "success" => $this->getJson($request)
+        ]);
+    }
+    
+     /**
+     * @Route("/login", name="login", methods={"POST"})
+     * @SWG\Post(
+     *    tags={"Authorization"},
+     *    summary="User login",
+     *     @SWG\Parameter(
+     *         name="body",
+     *         in="body",
+     *         description="Request body with email and password",
+     *         required=true,
+     *         @SWG\Schema(
+     *             @SWG\Property(property="email", type="string", example="user@host.zone"),
+     *             @SWG\Property(property="password", type="string", example="password")
+     *         )
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Start login proccess"
+     *     ),
+     * )
+     */
+    public function login(Request $request)
+    {
+        $data = $this->getJson($request);
+
+        return $this->json([
+            "success" => $data
+        ]);
+    }
+
+
+    /**
+     * @Route("/logout", name="logout", methods={"POST"})
+     * @SWG\Post(
+     *    tags={"Authorization"},
+     *    summary="User logout",
+     *     @SWG\Parameter(
+     *         name="body",
+     *         in="body",
+     *         description="Request with email user",
+     *         required=true,
+     *         @SWG\Schema(
+     *             @SWG\Property(property="email", type="string", example="user@host.zone"),
+     *         )
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Start login proccess"
+     *     ),
+     * )
+     */
+    public function logout(Request $request)
+    {
+        $data = $this->getJson($request);
+
+        return $this->json([
+            "success" => $data
+        ]);
+    }
+
+     /**
+     * @Route("/password/reset", name="passwordReset", methods={"POST"})
+     * @SWG\Post(
+     *    tags={"Authorization"},
+     *    summary="Password reset",
+     *     @SWG\Parameter(
+     *         name="body",
+     *         in="body",
+     *         description="Request for password reset",
+     *         required=true,
+     *         @SWG\Schema(
+     *             @SWG\Property(property="new_password", type="string", example="qwerty"),
+     *             @SWG\Property(property="old_password", type="string", example="123456")
+     *         )
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Start login proccess"
+     *     ),
+     * )
+     */
+    public function passwordReset(Request $request)
+    {
+        $data = $this->getJson($request);
+
+        return $this->json([
+            "success" => $data
+        ]);
+    }
+
+    /**
+     * @Route("/password/reset/complete", name="passwordResetComplete", methods={"POST"})
+     * @SWG\Post(
+     *    tags={"Authorization"},
+     *    summary="Password reset",
+     *     @SWG\Parameter(
+     *         name="body",
+     *         in="body",
+     *         description="Request for password reset",
+     *         required=true,
+     *         @SWG\Schema(
+     *             @SWG\Property(property="message", type="string", example="Password has been changed"),
+     *         )
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Start login proccess"
+     *     ),
+     * )
+     */
+    public function passwordResetComplete(Request $request)
+    {
+        $data = $this->getJson($request);
+
+        return $this->json([
+            "success" => $data
+        ]);
+    }
+    
 }
